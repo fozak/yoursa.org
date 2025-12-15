c4715341b2646dd61eba0d5fa97b12ce638d66d6
fozak <i771468@gmail.com>
2025-12-15 17:07:07 -0500

    QZ

--- assets/js/_loader.js
+++ assets/js/_loader.js
@@ -162,7 +162,7 @@ function loadSidebarImage() {
             const mainContent = document.getElementById('main-content');
             mainContent.insertAdjacentElement('afterbegin', sidebar); // Inside main-content
             
-            mainContent.style.marginLeft = '30%';
+            /*mainContent.style.marginLeft = '30%';*/
         };
         pngImg.src = pngPath;
     };
